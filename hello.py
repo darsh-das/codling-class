bf3f0d925174c421085fdab262871a59e1369723
darsh-das <darshshibudas@gmail.com>
2025-08-10 22:08:46 +0400

    first ai

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello world! I am Darsh")
+name=input("enter your name")
+print(f"Nice to meet you, {name}")
+print("how are you feeling today?")
+mood=input()
+if mood=="good":
+    print("I am glad to hear that")
+if mood=="bad":
+    print("am sorry to here that, hope thing better soon")
+if mood=="hungry":
+    print("have some food please")
+else:
+    print("i see. sometimes it's hard to put feeling into words")
